148ad9a83d3e05c87594bea0f1352cf9091736d1
arpit-uz <arpit@unizen.io>
2022-03-15 12:00:01 +0530

    net change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const defaultSpender = "0x207ca4370639120f9A049aF9CAB4fCaa608F2445";
 const nonce = 1;
 const defaultSender = "0xFd71Dc9721d9ddCF0480A582927c3dCd42f3064C";
 const chainId = 137;
-const usdcAddress = "0xDD9185DB084f5C4fFf3b4f70E7bA62123b812226"
+const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
 import {  getPermitDigest, PERMIT_TYPEHASH } from "../eip-2612/signatures.js"
 
 let provider = new HDWalletProvider(["e73de1205f0146c16f00c748709760a345f152660a2e7af5abf6a61048b9554b","e9c730cfba8b1d0cf7428cd2be8cb52e0e20df83d92db78201a4ee68122e1cec"],"https://polygon-mainnet.g.alchemy.com/v2/UxNKJculPwa1AqNx3omJhXDRVO9wWuMj"
